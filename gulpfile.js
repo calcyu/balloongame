bfac19e369fd091b9a6c9752fe29b7ed9bc2c653
求愚 <calcyu@qq.com>
2023-03-09 09:13:06 +0000

    fix: js filename error

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { series, src, dest } = require('gulp');
 const clean = require('gulp-clean');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const inject = require('gulp-inject');
 
 function clear() {
     return src('dist', { allowEmpty: true })
@@ -19,6 +20,12 @@ function js() {
 }
 function html() {
     return src('src/**/*.html')
+        .pipe(inject(src(['src/js/**/*.js'], { read: false }), {
+            relative: true,
+            transform: function (filepath, file) {
+                return `<script src="${filepath.substring(0, filepath.lastIndexOf('.'))}.min.js"></script>`;
+            }
+        }))
         .pipe(dest('dist/'));
 }
 
